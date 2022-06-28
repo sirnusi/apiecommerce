9e189fdc61548ef05d9c88a8ec832741b1d4c48b
Sanusi David <sanusidavid360@gmail.com>
2022-06-28 12:35:27 +0100

    ecom

--- ecommerce_app/models.py
+++ ecommerce_app/models.py
@@ -5,7 +5,7 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     
     class Meta:
-        verbose_name_plural = "Category"
+        verbose_name_plural = "Categories"
     
     def __str__(self):
         return self.name
