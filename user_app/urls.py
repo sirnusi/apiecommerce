bc27b813d76dfd651df340c10f1954e1d0465dca
Sanusi David <sanusidavid360@gmail.com>
2022-06-28 17:23:59 +0100

    ecom

--- user_app/urls.py
+++ user_app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import views
 
 urlpatterns = [
+    path('register/', views.registration_view, name='register'),
+    
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
