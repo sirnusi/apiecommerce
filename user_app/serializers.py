3513f33c96c0285c485f1b3063d2e719529d7a60
Sanusi David <sanusidavid360@gmail.com>
2022-06-28 17:21:29 +0100

    ecom

---
+++ user_app/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    password2 = serializers.CharField(write_only=True, style={'input_type':'password'})
+    
+    class Meta:
+        model = User
+        fields = ["username", 'email', 'password', 'password2']
+        extra_kwargs = {
+            'password':{'write_only':True}
+        }
+    
+    def save(self):
+        password = self.validated_data['password']
+        password2 = self.validated_data['password2']
+        
+        if password != password2:
+            raise serializers.ValidationError({'error':'P1 and P2 should be the same!'})
+        
+        query = User.objects.filter(email=self.validated_data['email'])
+        if query.exists():
+            raise serializers.ValidationError({'error':'Email already exist'})
+        
+        account = User(email=self.validated_data['email'], username=self.validated_data['username'])
+        account.setpassword(password)
+        account.save()
+        
+        return account
+        
--- user_app/views.py
+++ user_app/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from .serializers import RegistrationSerializer
+from rest_framework.decorators import api_view
+from rest_framework_simplejwt.tokens import RefreshToken
 
 # Create your views here.
+@api_view(['POST'])
+def registration_view(request):
+    
+    if request.method == 'POST':
+        serializer = RegistrationSerializer(data=request.data)
+        
+        data = {}
+        
+        if serializer.is_valid():
+            account = serializer.save()
+            
+            data['Response'] = 'Registration Successful'
+            data['username'] = account.username
+            data['email'] = account.email
+            
+            refresh = RefreshToken.for_user(account)
+            data['tokens'] = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }
+        else:
+            data = serializer.errors
+                
+        return Response(data)
