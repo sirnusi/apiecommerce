50bdd2c7bbf9cca4908b751cb9255eda1fdf5d6a
Sanusi David <sanusidavid360@gmail.com>
2022-07-04 20:48:05 +0100

    ecom

--- ecommerce_app/views.py
+++ ecommerce_app/views.py
@@ -35,11 +35,17 @@ class ProductCreateAV(CreateAPIView):
         serializer.save(owner=self.request.user)
    
 class ProductDetailAV(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     serializer_class = ProductSerializer
     permission_classes = [ProductOwnerOrReadOnly]
-    # only the people that created that can delete it but anybody can read it.
+    
+    def get_queryset(self):
+        return Product.objects.all()
 
+    def perform_update(self, serializer):
+        
+        pass
+        
 
 class CategoryListAV(ListAPIView):
     queryset = Category.objects.all()
