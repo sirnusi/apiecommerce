1a174b6a49d285cf1950eb5af47303acf6057840
Sanusi David <sanusidavid360@gmail.com>
2022-07-12 13:37:35 +0100

    ecom

--- ecommerce_app/models.py
+++ ecommerce_app/models.py
@@ -1,16 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator, MaxValueValidator
-
-class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    
-    
-
-    def __str__(self):
-        return self.name
     
     
 class Category(models.Model):
@@ -40,7 +30,7 @@ class Product(models.Model):
 class Review(models.Model):
     rating = models.PositiveIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     description = models.CharField(max_length=500)
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='product')
     active = models.BooleanField(default=True)
     
@@ -48,8 +38,7 @@ class Review(models.Model):
         return str(self.rating) | self.product.name
 
 class Order(models.Model):
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, 
-                                 blank=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateTimeField(auto_now_add=True)
     complete = models.BooleanField(default=False, null=True, blank=False)
     transaction_id = models.CharField(max_length=100)
