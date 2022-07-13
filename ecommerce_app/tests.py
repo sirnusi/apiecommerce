a176ac19b34aaeaa31230c823591098b293d18b4
Sanusi David <sanusidavid360@gmail.com>
2022-07-13 12:23:17 +0100

    ecom

--- ecommerce_app/tests.py
+++ ecommerce_app/tests.py
@@ -4,11 +4,12 @@ from django.urls import reverse
 from django.contrib.auth.models import User
 # Create your tests here.
 
-class CategoryTestCase(test.APITestCase):
+# class CategoryTestCase(test.APITestCase):
     
-    def setUp(self):
-        self.user = User.objects.create(username='sirnusi', password='Newpassword123')
-    
-    def test_category(self):
+#     # def setUp(self):
+    #     self.user = User.objects.create(username='sirnusi', password='Newpassword123')
         
-        pass
+    
+    # def test_category(self):
+        
+    #     pass
--- user_app/tests.py
+++ user_app/tests.py
@@ -1,7 +1,7 @@
 from rest_framework import status, test
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+from rest_framework_simplejwt import tokens
 
 # Create your tests here.
 class RegistrationTestCase(test.APITestCase):
@@ -15,3 +15,25 @@ class RegistrationTestCase(test.APITestCase):
         }
         response = self.client.post(reverse('register'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        
+        
+class LoginLogoutTestCase(test.APITestCase):
+    
+    def setUp(self):
+        self.user = User.objects.create(username='sirnusi',
+                                        password='Newpassword123'
+                                        )
+        
+        self.refresh = tokens.RefreshToken.for_user(self.user)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(self.refresh.access_token))
+       
+
+    def test_login(self):
+        data = {
+            'username': 'sirnusi', 
+            'password': 'Newpassword123',
+        }
+             
+        response = self.client.post(reverse('token_obtain_pair'), data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        
